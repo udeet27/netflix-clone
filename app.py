927b0809024ed0fc6ffba38675ee597a01789d66
Udeet Mittal <udeet1575@gmail.com>
2025-02-13 18:50:14 +0530

    asd

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import requests
 from flask_cors import CORS
 from werkzeug.middleware.proxy_fix import ProxyFix
 from requests.adapters import HTTPAdapter
-from requests.packages.urllib3.util.retry import Retry
 import time
 
 app = Flask(__name__)
