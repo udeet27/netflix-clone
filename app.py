fed547cd82cbb8d9f087da0cc4af2ddcb5f7a0a5
Udeet Mittal <udeet1575@gmail.com>
2025-02-17 17:24:23 +0530

    as

--- app.py
+++ app.py
@@ -98,10 +98,11 @@ def get_free_proxy():
 
 
 def try_search_with_fallback(query, find_all=True):
-    proxy = FreeProxy().get()
+    
 
     for domain in HDREZKA_DOMAINS:
         try:
+            proxy = FreeProxy().get()
             headers = {
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
                 "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
